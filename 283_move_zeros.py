af2cfc7b13e99e9165349fb11ab347249383ca03
Ishaan S. Oshima <ishaansoshima@gmail.com>
2024-07-18 23:18:01 +0530

    day3-2nd step

---
+++ 283_move_zeros.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        j = 0
+        for num in nums:
+            if num != 0:
+                nums[j] = num
+                j += 1
+
+        for i in range(j, len(nums)):
+            nums[i] = 0
+        
